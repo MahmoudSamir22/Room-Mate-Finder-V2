d5b719e3765e24591a666e0cb73e38c10cc368cc
Mahmoud Samir <menatosamir55@gmail.com>
2022-06-26 18:53:51 +0200

    adding secur config

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ require("dotenv").config({ path: "./config.env" });
 const cors = require("cors");
 const {webHookCheckOut} = require('./controllers/userController')
 const compression = require('compression')
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean')
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit')
 
 const mountRoutes = require("./routes/index");
 const globalError = require("./middleware/errorsMiddleware");
@@ -28,6 +32,25 @@ app.post('/sub-webhook', express.raw({type: 'application/json'}),  webHookCheckO
 
 app.use(express.json({ limit: "20kb" }));
 app.use(express.static(path.join(__dirname, "uploads")));
+
+
+// Data cleaning and sanitization
+app.use(mongoSanitize());
+app.use(xss())
+
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, 
+  message: 'Too many accounts created from this IP, please try again after an hour'
+})
+
+// Apply the rate limiting middleware to all requests
+app.use('/api', limiter)
+
+
+//Middleware to protect against HTTP paramter pollution attacks
+app.use(hpp({whitelist: ['roomPrice']}));
 
 mountRoutes(app);
 app.use("*", (req, res, next) => {
